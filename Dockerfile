d60a92837964ed8bad9903a43297b9ba3b8ce4b0
Jesús Alejandro Yahuitl Rodríguez <jesusaleyaro@hotmail.com>
2021-10-05 20:22:25 -0500

    deploy covid.jesusyaro.com

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM python:3.8-slim-buster
 
 WORKDIR /app
 
-RUN pip3 install flask sklearn pandas
+RUN pip3 install flask sklearn pandas waitress
 
 COPY . .
 
--- main.py
+++ main.py
@@ -8,7 +8,6 @@ rna.fit()
 app = flask.Flask(__name__)
 #app.config["DEBUG"] = True
 
-
 @app.route('/', methods=['GET'])
 def home():
     return render_template('index.html')
@@ -21,4 +20,6 @@ def test():
 def predict():
     return {'result' : str(rna.predict(request.args['sexo'], request.args['entidad_res'], request.args['neumonia'], request.args['edad'], request.args['diabetes'], request.args['epoc'], request.args['asma'], request.args['inmusupr'], request.args['hipertension'], request.args['otra_com'], request.args['cardiovascular'], request.args['obesidad'], request.args['renal_cronica'], request.args['tabaquismo'], request.args['otro_caso'], request.args['clasificacion_final']))}
 
-app.run(debug=False,host='0.0.0.0')
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=80)
